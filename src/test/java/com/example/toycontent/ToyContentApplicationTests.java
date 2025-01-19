9778a0827f04194631993834fbdc6f91b150d2aa
Minchae0322 <jmcabc0322@gmail.com>
2025-01-19 23:05:27 +0900

    setting: init project

---
+++ src/test/java/com/example/toycontent/ToyContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toycontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
