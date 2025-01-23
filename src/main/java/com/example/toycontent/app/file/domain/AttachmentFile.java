a967c5b7a407992d78b130032181de96017c0982
Minchae0322 <jmcabc0322@gmail.com>
2025-01-23 23:39:02 +0900

    feat: 파일 엔티티 정의

---
+++ src/main/java/com/example/toycontent/app/file/domain/AttachmentFile.java
@@ -0,0 +1,57 @@
+package com.example.toycontent.app.file.domain;
+
+
+import com.example.toycontent.app.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@SuperBuilder
+@DynamicInsert
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "attch_file_tbl")
+@EntityListeners(AuditingEntityListener.class)
+public class AttachmentFile extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attch_file_id", unique = true, nullable = false)
+    @Comment("첨부 파일 아이디")
+    private Long id;
+
+    @Column(length = 50)
+    @Enumerated(EnumType.STRING)
+    @Comment("파일 코드")
+    private String fileCode;
+
+    @Column(name = "org_file_nm")
+    @Comment("원본 파일 명")
+    private String orgFileNm;
+
+    @Column(name = "store_file_nm")
+    @Comment("저장 파일 명")
+    private String storeFileNm;
+
+    @Column(length = 2000)
+    @Comment("파일 url")
+    private String fileUrl;
+
+    @Comment("파일 사이즈")
+    private Long fileSize;
+
+    @Column(name = "file_explain", length = 2000)
+    @Comment("파일 설명")
+    private String fileExplain;
+
+}
