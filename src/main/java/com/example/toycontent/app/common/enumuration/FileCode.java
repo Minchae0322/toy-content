6721aaec087275f3c7b565f1127bc9c477244ff6
Minchae0322 <jmcabc0322@gmail.com>
2025-01-26 17:31:36 +0900

    fix: fix file path

--- src/main/java/com/example/toycontent/app/common/enumuration/FileCode.java
+++ src/main/java/com/example/toycontent/app/common/enumuration/FileCode.java
@@ -1,4 +1,29 @@
 package com.example.toycontent.app.common.enumuration;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum FileCode {
+
+    PRODUCT("PRODUCT", "제품 업로드 파일"),
+    ;
+
+    private String title;
+    private String description;
+
+    public static FileCode ofCode(String inputCode) {
+        return Arrays.stream(values())
+                .filter(v -> v.title.equals(inputCode))
+                .findAny()
+                .orElseThrow(
+                        () ->
+                                new IllegalArgumentException(
+                                        String.format("statusCode의 code 값이 올바르지 않습니다. 입력 값 : %s", inputCode)));
+    }
 }
--- src/main/java/com/example/toycontent/app/file/controller/AttachmentFileController.java
+++ src/main/java/com/example/toycontent/app/file/controller/AttachmentFileController.java
@@ -6,11 +6,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "AttachmentFileController", description = "첨부파일 API")
@@ -22,7 +20,7 @@ public class AttachmentFileController {
 
     private final FileService fileService;
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "파일 업로드", description = "사용자가 파일을 업로드합니다.")
     public ResponseEntity<UploadFileDto> uploadFile(@RequestPart("file") MultipartFile file) {
         return ResponseEntity.ok(fileService.uploadFile(file));
