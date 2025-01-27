029751f22c99e019e22204136ce1787d3e9e405d
Minchae0322 <jmcabc0322@gmail.com>
2025-01-28 00:22:20 +0900

    setting: spring securityConfig setting

--- src/main/java/com/example/toycontent/app/common/JwtParser.java
+++ src/main/java/com/example/toycontent/app/common/JwtParser.java
@@ -1,24 +1,34 @@
 package com.example.toycontent.app.common;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class JwtParser {
 
-    private final String secretKey = "your-secret-key"; // 변경: 환경 변수 또는 설정 파일로 관리
+    public static final Long ACCESS_TOKEN_EXPIRATION = 60 * 60 * 1000L;
+    public static final Long REFRESH_TOKEN_EXPIRATION = 30 * 24 * 60 * 60 * 1000L;
 
-    public Claims getClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey.getBytes())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    public Long getUserId(String token) {
-        Claims claims = getClaims(token);
-        return claims.get("userId", Long.class); // "userId" 클레임에 사용자 ID 저장
-    }
 }
