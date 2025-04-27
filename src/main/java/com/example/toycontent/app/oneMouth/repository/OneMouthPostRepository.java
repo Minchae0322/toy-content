63d7ee49e96954c5b48628eadbc5e4f3971b252b
Minchae0322 <jmcabc0322@gmail.com>
2025-04-27 16:52:03 +0900

    feat: one mouth and attachment file mapping class

--- src/main/java/com/example/toycontent/app/oneMouth/repository/OneMouthPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.toycontent.app.oneMouth.repository;
-
-import com.example.toycontent.app.oneMouth.domain.OneMouthPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OneMouthPostRepository extends JpaRepository<OneMouthPost, Long> {
-}
---
+++ src/main/java/com/example/toycontent/app/oneMouth/repository/OneMouthRepository.java
@@ -0,0 +1,7 @@
+package com.example.toycontent.app.oneMouth.repository;
+
+import com.example.toycontent.app.oneMouth.domain.OneMouth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OneMouthRepository extends JpaRepository<OneMouth, Long> {
+}
