c74e2d5acd387d169a812013b4e8cefa1b720fd2
Minchae0322 <jmcabc0322@gmail.com>
2025-05-24 21:23:04 +0900

    feat: create post and draft post

--- src/main/java/com/example/toycontent/app/oneMouth/controller/OneMouthController.java
+++ src/main/java/com/example/toycontent/app/oneMouth/controller/OneMouthController.java
@@ -1,6 +1,7 @@
 package com.example.toycontent.app.oneMouth.controller;
 
 import com.example.toycontent.app.oneMouth.controller.dto.OneMouthCreateDto;
+import com.example.toycontent.app.oneMouth.controller.dto.OneMouthDraftCreateDto;
 import com.example.toycontent.app.oneMouth.service.OneMouthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,8 +21,16 @@ public class OneMouthController {
     @PostMapping("")
     @Operation(summary = "한입만 게시물 생성", tags = "OneMouthController")
     public ResponseEntity<Long> createOneMouth(
-            @RequestBody @Valid OneMouthCreateDto oneMouthCreateDto
+            @RequestBody @Valid OneMouthCreateDto createDto
     ) {
-        return ResponseEntity.ok(oneMouthService.createOneMouth(oneMouthCreateDto));
+        return ResponseEntity.ok(oneMouthService.createOneMouth(createDto));
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "한입만 게시물 임시저장", tags = "OneMouthController")
+    public ResponseEntity<Long> createOneMouthDraft(
+            @RequestBody @Valid OneMouthDraftCreateDto createDto
+    ) {
+        return ResponseEntity.ok(oneMouthService.createOneMouthDraft(createDto));
     }
 }
