a0964261009caf3ddb9e15800c70a293f445883a
Minchae0322 <jmcabc0322@gmail.com>
2025-06-03 23:15:23 +0900

    feat: create one-mouth post draft

--- src/main/java/com/example/toycontent/app/oneMouth/service/OneMouthService.java
+++ src/main/java/com/example/toycontent/app/oneMouth/service/OneMouthService.java
@@ -4,9 +4,13 @@ import com.example.toycontent.app.category.domain.Category;
 import com.example.toycontent.app.category.repository.CategoryRepository;
 import com.example.toycontent.app.oneMouth.controller.dto.OneMouthCreateDto;
 import com.example.toycontent.app.oneMouth.controller.dto.OneMouthDraftCreateDto;
+import com.example.toycontent.app.oneMouth.domain.OneMouthDraft;
+import com.example.toycontent.app.oneMouth.repository.OneMouthDraftRepository;
 import com.example.toycontent.app.oneMouth.repository.OneMouthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,7 @@ public class OneMouthService {
 
     private final OneMouthRepository oneMouthRepository;
     private final CategoryRepository categoryRepository;
+    private final OneMouthDraftRepository draftRepository;
 
     public Long createOneMouth(OneMouthCreateDto createDto) {
         Category category = categoryRepository.findById(createDto.getCategoryId()).orElseThrow();
@@ -24,6 +29,12 @@ public class OneMouthService {
     public Long createOneMouthDraft(OneMouthDraftCreateDto draftCreateDto) {
         Category category = categoryRepository.findById(draftCreateDto.getCategoryId()).orElseThrow();
 
-        return oneMouthRepository.save(draftCreateDto.toEntity(category)).getId();
+        Optional<OneMouthDraft> optionalDraft = draftRepository.findBySellerId(draftCreateDto.getSellerId());
+
+        OneMouthDraft draft = optionalDraft
+                .map(OneMouthDraft::update)
+                .orElseGet(() -> draftRepository.save(draftCreateDto.toEntity(category)));
+
+        return draft.getId();
     }
 }
