3156bf71f581a86e5ac51ecc184b4e73e555bafb
Minchae0322 <jmcabc0322@gmail.com>
2025-06-03 23:15:23 +0900

    feat: create one-mouth post draft

--- src/main/java/com/example/toycontent/app/oneMouth/domain/OneMouthDraft.java
+++ src/main/java/com/example/toycontent/app/oneMouth/domain/OneMouthDraft.java
@@ -1,6 +1,7 @@
 package com.example.toycontent.app.oneMouth.domain;
 
 
+import com.example.toycontent.app.category.domain.Category;
 import com.example.toycontent.app.common.enumuration.Unit;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -47,7 +49,28 @@ public class OneMouthDraft {
     @Enumerated(EnumType.STRING)
     private Unit unit;
 
+    @JoinColumn(name = "category_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Comment("카테고리")
+    private Category category;
+
+    @Column(nullable = false)
+    @Comment("판매자 아이디")
+    private Long sellerId;
+
+    @Column(nullable = false, updatable = false)
+    @Comment("생성 시간")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @Comment("수정 시간")
+    private LocalDateTime updatedAt;
+
     @OneToMany
     @JoinColumn(name = "one_mout_attachment_file_id")
     private List<OneMouthAttachmentFile> oneMouthAttachmentFiles;
+
+    public OneMouthDraft update() {
+        return this;
+    }
 }
