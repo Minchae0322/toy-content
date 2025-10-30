fa880de0fb31f68139a131e2dbc6b4a7297b5330
Minchae0322 <jmcabc0322@gmail.com>
2025-10-30 18:05:17 +0900

    feat: 배틀 시스템 기본 구조 구현 (WIP)
    - 배틀 아이템 추가

--- src/main/java/com/example/toycontent/app/battle/repository/BattleItemRepository.java
+++ src/main/java/com/example/toycontent/app/battle/repository/BattleItemRepository.java
@@ -1,8 +1,10 @@
 package com.example.toycontent.app.battle.repository;
 
+import com.example.toycontent.app.battle.domain.Battle;
 import com.example.toycontent.app.battle.domain.BattleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BattleItemRepository extends JpaRepository<BattleItem, Long> {
 
+  long countByBattleAndIsDeletedFalse(Battle battle);
 }
