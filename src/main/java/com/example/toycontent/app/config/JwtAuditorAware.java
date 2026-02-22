a7b6d52fb32915003243279cd695a204278a8447
minchae <jmcabc0322@gmail.com>
2026-02-22 18:33:20 +0900

    fix: JwtAuditorAware 수정

--- src/main/java/com/example/toycontent/app/config/JwtAuditorAware.java
+++ src/main/java/com/example/toycontent/app/config/JwtAuditorAware.java
@@ -5,6 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Optional;
 
@@ -14,26 +17,27 @@ import static com.example.toycontent.app.common.constants.GlobalConstants.AUTHOR
 @RequiredArgsConstructor
 public class JwtAuditorAware implements AuditorAware<Long> {
 
-    private final HttpServletRequest httpServletRequest;
     private final JwtParser jwtParser;
-
 
     @Override
     public Optional<Long> getCurrentAuditor() {
-        // Authorization 헤더에서 JWT 토큰 추출
-        String token = httpServletRequest.getHeader(AUTHORIZATION_HEADER);
-
-        if (token == null || !token.startsWith("Bearer ")) {
-            return Optional.empty();
-        }
-
-        // "Bearer " 제거 후 토큰 파싱
-        token = token.substring(7);
-
         try {
-            return Optional.of(null);
+            RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+            if (attributes == null) {
+                return Optional.empty();
+            }
+
+            HttpServletRequest request = ((ServletRequestAttributes) attributes).getRequest();
+            String token = request.getHeader(AUTHORIZATION_HEADER);
+
+            if (token == null || !token.startsWith("Bearer ")) {
+                return Optional.empty();
+            }
+
+            token = token.substring(7);
+            return Optional.of(jwtParser.getUserId(token));
         } catch (Exception e) {
-            return Optional.empty(); // 토큰이 유효하지 않을 경우
+            return Optional.empty();
         }
     }
 }
