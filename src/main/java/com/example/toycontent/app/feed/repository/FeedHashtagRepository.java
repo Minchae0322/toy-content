84ad67404b26e23bd8d4048784e5508b3c71608f
Minchae0322 <jmcabc0322@gmail.com>
2026-02-25 14:48:04 +0900

    fix: 피드 수정 시 해시태그 오류 수정

--- src/main/java/com/example/toycontent/app/feed/repository/FeedHashtagRepository.java
+++ src/main/java/com/example/toycontent/app/feed/repository/FeedHashtagRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedHashtagRepository extends JpaRepository<FeedHashtag, Long> {
 
+  void deleteAllByFeed_Id(Long feedId);
 }
