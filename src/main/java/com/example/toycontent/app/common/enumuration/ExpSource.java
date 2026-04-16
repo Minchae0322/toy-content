d8a4dd9215338b724b4641515f48b238b2f55cf5
Minchae0322 <jmcabc0322@gmail.com>
2026-04-16 17:48:53 +0900

    feat: EXP 적립 이력 테이블 추가 및 addExp 이력 저장 통합
    ExpHistory 엔티티와 ExpSource enum을 추가하여 경험치 변동 추적 가능하도록 구성

---
+++ src/main/java/com/example/toycontent/app/common/enumuration/ExpSource.java
@@ -0,0 +1,20 @@
+package com.example.toycontent.app.common.enumuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ExpSource {
+
+  FEED_CREATE("피드 작성"),
+  COMMENT_CREATE("댓글 작성"),
+  BATTLE_VOTE("배틀 투표"),
+  BATTLE_PREDICTION_HIT("배틀 예측 적중"),
+  MISSION_CLAIM("일일 미션 보상 수령"),
+  STREAK_BONUS("연속 출석 보너스"),
+  ADMIN_GRANT("관리자 지급"),
+  ;
+
+  private final String description;
+}
