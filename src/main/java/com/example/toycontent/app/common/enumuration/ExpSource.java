1a3a161ea0a6b348ee2f725d9bce9f4720dc077d
Minchae0322 <jmcabc0322@gmail.com>
2026-04-23 15:53:04 +0900

    feat(reward): 배틀 아이템 추가 시 EXP 20 지급 (배틀당 최대 5회)
    - ExpSource.BATTLE_ITEM_ADD 추가 (20 EXP, 일일 캡 적용)
    - ExpGrantService.grantWithCountCap 추가: sourceId별 지급 횟수 제한
    - addBattleItems 호출 시 배틀당 최대 5회까지 지급

--- src/main/java/com/example/toycontent/app/common/enumuration/ExpSource.java
+++ src/main/java/com/example/toycontent/app/common/enumuration/ExpSource.java
@@ -7,16 +7,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ExpSource {
 
-  FEED_CREATE("피드 작성", 20L, false),
+  FEED_CREATE("피드 작성", 30L, false),
   COMMENT_CREATE("댓글 작성", 5L, false),
-  BATTLE_VOTE("배틀 투표", 5L, false),
-  BATTLE_PREDICTION_HIT("배틀 예측 적중", 30L, false),
+  BATTLE_VOTE("배틀 투표", 10L, false),
+  BATTLE_ITEM_ADD("배틀 아이템 추가", 20L, false),
+  BATTLE_PREDICTION_HIT("배틀 예측 적중", 100L, false),
   MISSION_CLAIM("일일 미션 보상 수령", 0L, true),
   STREAK_BONUS("연속 출석 보너스", 0L, true),
   ADMIN_GRANT("관리자 지급", 0L, true),
   FEED_REACTION("피드 리액션 수신", 5L, false),
   BATTLE_WEIGHTED_VOTE("가중 투표", 10L, false),
-  PICK_COMMENT("PICK 한마디", 10L, false),
+  PICK_COMMENT("PICK 한마디", 5L, false),
   HOT_DISCOVER("HOT 발굴자", 50L, false),
   WEEKLY_RANKING("주간 랭킹", 0L, true),
   BATTLE_RESULT("배틀 결과", 0L, true),
